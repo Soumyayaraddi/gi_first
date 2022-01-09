8a20d351097671a00dbad73ca8d8ce6f43e4a56f
soumyaky2000@gmail.com <soumyaky2000@gmail.com>
2022-01-09 12:11:35 -0800

    second commit

--- simple.java
+++ simple.java
@@ -3,6 +3,8 @@ public class simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+                System.out.println("this is soumya");
+
 
 	}
 
